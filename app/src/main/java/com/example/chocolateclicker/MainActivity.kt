dd1cb9fb14ee5bd725dea16e5a2151e6581c8f73
ilya <ikonacenko0@gmail.com>
2022-10-16 23:22:52 +0400

    minor

--- app/src/main/java/com/example/chocolateclicker/MainActivity.kt
+++ app/src/main/java/com/example/chocolateclicker/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.chocolateclicker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shopBtnAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.shop_btn_anim)
+        val shopBtn: Button = findViewById(R.id.shopBtn2)
+        shopBtn.setOnClickListener { v ->
+            v.startAnimation(shopBtnAnim)
+            val shopIntent = Intent(this, Shop::class.java)
+            startActivity(shopIntent)
+        }
     }
 }
--- app/src/main/java/com/example/chocolateclicker/Shop.kt
+++ app/src/main/java/com/example/chocolateclicker/Shop.kt
@@ -1,11 +1,20 @@
 package com.example.chocolateclicker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class Shop : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop)
+
+        val backBtn: ImageView = findViewById(R.id.backBtn)
+        backBtn.setOnClickListener { v ->
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
     }
 }
